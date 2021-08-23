d0f115f218ae6945746ed7a46632b5b036a9cf04
DmitriyTishchenko <kt041084tdn@gmail.com>
2021-08-23 23:56:08 +0300

    If and Switch

--- src/Main3IfSwitch.java
+++ src/Main3IfSwitch.java
@@ -1,11 +1,30 @@
 public class Main3IfSwitch {
     public static void main(String[] args) {
-        int a = 100;
+        int a = 10;
         int b = 20;
 
         if (a > b) {
             System.out.println("True");
+        } else {
+            System.out.println("False");
         }
 
+        int grade = 5;
+        switch (grade) {
+            case 5:
+                System.out.println("Best");
+                break;
+            case 4:
+                System.out.println("Good");
+                break;
+            case 3:
+                System.out.println("Norm");
+                break;
+            case 2:
+                System.out.println("Bad");
+                break;
+            default:
+                System.out.println("Out of range");
+        }
     }
 }
